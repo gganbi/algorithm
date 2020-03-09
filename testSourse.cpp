6b5269c48bb07337ee62e66a91d9bef57409b3d5
gganbi <gganbi@naver.com>
2020-03-09 15:06:26 +0900

    c++소스모음

---
+++ testSourse.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main() {
+
+	int a[100];
+
+	int i = 1, j = 1;
+	int max = 0;
+
+
+	for (i = 1; i <= 9; i++) {
+		scanf_s("%d", &a[i]);
+	}
+
+	max = a[1];
+
+
+	for (i = 1; i <= 9; i++) {
+		if (max <= a[i]) {
+			max = a[i];
+			j = i;
+		}
+	}
+
+
+		printf("%d\n", max);
+		printf("%d ", j);
+
+
+	
+	return 0;
+}

536694cdbdb9c86c09cc115c2c2c7642e64d262e
gganbi <gganbi@naver.com>
2020-03-10 14:25:24 +0900

    펠린드롬문제 수정필요

--- string3.cpp
+++ string3.cpp
@@ -1,21 +1,21 @@
-#include<stdio.h>
-#include<string.h>
-
-int main() {
-
-	char a[100001];
-	char b[100001];
-	int i, j;
-	int count = 0;
-
-	fgets(a, sizeof(a), stdin);
-
-	for (j = strlen(a); j >=0; j--) {
-		printf("%c", a[j]);
-	}
-
-
-
-
-	return 0;
-}
+//#include<stdio.h>
+//#include<string.h>
+//
+//int main() {
+//
+//	char a[100001];
+//	char b[100001];
+//	int i, j;
+//	int count = 0;
+//
+//	fgets(a, sizeof(a), stdin);
+//
+//	for (j = strlen(a); j >=0; j--) {
+//		printf("%c", a[j]);
+//	//}
+//
+//
+//
+//
+//	return 0;
+//}

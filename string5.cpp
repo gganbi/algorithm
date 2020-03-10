accc90a48aa84f00d90997d76728cecb7a3d90a3
gganbi <gganbi@naver.com>
2020-03-11 08:59:36 +0900

    해당문제 배열인덱스로 다시 풀었음

---
+++ string5.cpp
@@ -0,0 +1,34 @@
+//#include<stdio.h>
+//#include<string.h>
+//
+//int main() {
+//
+//	char a[100001];
+//	char b[100001];
+//	int i, j;
+//	int length = 0;
+//	bool tmp = false;
+//	fgets(a, sizeof(a), stdin);
+//
+//	length = strlen(a) - 1;
+//
+//	for (i = 0; i < length / 2; i++) {
+//		if (a[i] != a[length - i - 1]) {
+//			tmp = true;
+//			break;
+//		}
+//	}
+//	if (tmp == true) {
+//		printf("NO");
+//	}
+//	else {
+//		printf("YES");
+//	}
+//
+//
+//
+//
+//
+//
+//	return 0;
+//}
--- test.cpp
+++ test.cpp
@@ -1,32 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main() {
 
-	int aa[101][101] = {0};
+	//Please Enter Your Code Here
+	int aa[101][101] = { 0 };
 
 	int i = 0, j = 0;
 	int a = 0;
 	int count = 1;
 
-		
+
+
 	scanf_s("%d", &a);
-	
 
 	for (i = 1; i <= a; i++) {
-	
 		for (j = 1; j <= i; j++) {
-			aa[j][i-j+1] = count++;
-		}	
+			aa[i][a - j + 1] = count++;
+		}
 	}
 
+
 	for (i = 1; i <= a; i++) {
-		for (j = 1; j <= a-i+1; j++) {
+		for (j = 1; j <= a - i + 1; j++) {
 			printf("%d ", aa[i][j]);
 		}printf("\n");
 	}
-
-	
-
 
 
 

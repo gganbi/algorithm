1e017ab1b4c060ccbee41eb18748f644ae4b5f22
gganbi <gganbi@naver.com>
2020-03-10 11:16:45 +0900

    array2

--- array2.cpp
+++ array2.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-/*
+
 int main() {
 
 	int aa[101][101] = {0};
@@ -7,26 +7,29 @@ int main() {
 	int i = 0, j = 0;
 	int a = 0,b=0;
 	int count = 1;
+	int c, d;
 
 
 	scanf_s("%d", &a);
 	scanf_s("%d", &b);
+	
+
 
 	for (i = 1; i <= a; i++) {
 		for (j = 1; j <= b; j++) {
-			aa[i][j] = count++;
+			scanf_s("%d", &aa[i][j]);
 		}
 	}
 
-	for (i = 1; i <= a; i++) {
-		for (j = 1; j <= b; j++) {
-			printf("%d ", aa[i][j]);
-		}printf("\n");
-	}
+
+	scanf_s("%d", &c);
+	scanf_s("%d", &d);
+
+
+			printf("%d ", aa[c+1][d+1]);
 
 
 
 
 	return 0;
 }
-*/

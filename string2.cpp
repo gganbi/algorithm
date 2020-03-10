bac5c9fd9a0fe5704ee393e4f6c7f9775731dd9a
gganbi <gganbi@naver.com>
2020-03-10 13:59:00 +0900

    문자열 뒤집기 수정필요

--- string2.cpp
+++ string2.cpp
@@ -1,29 +1,29 @@
-#include<stdio.h>
-
-int main() {
-
-	char a[100001];
-	char b[100001];
-	int i,j;
-	int count = 0;
-
-	fgets(a, sizeof(a), stdin);
-
-		for (j = 0; j < sizeof(a); j++) {
-			if (a[j] == ' ') {
-				
-			}
-			else {
-				b[count++] = a[j];
-			}
-		}
-
-
-
-
-	printf("%s", b);
-
-
-
-	return 0;
-}
+//#include<stdio.h>
+//
+//int main() {
+//
+//	char a[100001];
+//	char b[100001];
+//	int i,j;
+//	int count = 0;
+//
+//	fgets(a, sizeof(a), stdin);
+//
+//		for (j = 0; j < sizeof(a); j++) {
+//			if (a[j] == ' ') {
+//				
+//			}
+//			else {
+//				b[count++] = a[j];
+//			}
+//		}
+//
+//
+//
+//
+//	printf("%s", b);
+//
+//
+//
+//	return 0;
+//}

d1b88b1af6cb8056b9c2d5567cb2fc6cabef014c
gganbi <gganbi@naver.com>
2020-03-11 13:39:56 +0900

    course3 maxofarr

---
+++ cource3macofarr.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main() {
+
+	int data[10][10];
+	int max=0;
+	int i, j;
+	int x=1, y=1;
+
+	for (i = 1; i <= 9; i++) {
+		for (j = 1; j <= 9; j++) {
+			scanf_s("%d", &data[i][j]);
+			if (max <= data[i][j]) {
+				max = data[i][j];
+				x = i;
+				y = j;
+			
+			}
+		
+		}
+	}
+
+	printf("%d\n", max);
+	printf("%d %d",x,y );
+
+
+	
+
+
+
+	return 0;
+}
+
+
--- test.cpp
+++ test.cpp
@@ -1,33 +1,46 @@
-#include <stdio.h>
-
-int main() {
-
-	//Please Enter Your Code Here
-	int aa[101][101] = { 0 };
-
-	int i = 0, j = 0;
-	int a = 0;
-	int count = 1;
-
-
-
-	scanf_s("%d", &a);
-
-	for (i = 1; i <= a; i++) {
-		for (j = 1; j <= i; j++) {
-			aa[i][a - j + 1] = count++;
-		}
-	}
-
-
-	for (i = 1; i <= a; i++) {
-		for (j = 1; j <= a - i + 1; j++) {
-			printf("%d ", aa[i][j]);
-		}printf("\n");
-	}
-
-
-
-
-	return 0;
-}
+//#include<stdio.h>
+//
+//int main() {
+//
+//	int data[10] ;
+//    int a, b;
+//    int i;
+//	int temp = 0;
+//    int indexMin = 0;
+//
+//    scanf_s("%d", &a);
+//    scanf_s("%d", &b);
+//
+//    for (i = 0; i < a; i++) {
+//        scanf_s("%d", &data[i]);
+//    }
+//    for (int i = 0; i < a - 1; i++)
+//    {
+//        //중요
+//        indexMin = i;
+//        for (int j = i + 1; j < a; j++)
+//        {
+//            if (data[j] < data[indexMin])
+//            {    //data[indexMin]을 기준으로 data[j]를 검사한다.
+//                //data[indexMin]의 값 보다 작은 값을 발견시 indexMin = j가 된다.
+//                indexMin = j;
+//            }
+//        }
+//        //내부 for문의 반복으로 가장 작은 값을 찾으면 순서를 바꾼다.
+//        temp = data[indexMin];
+//        data[indexMin] = data[i];
+//        data[i] = temp;
+//    }
+//
+//    
+//    printf("%d ", data[a-b]);
+//
+//
+//	
+//
+//
+//
+//	return 0;
+//}
+//
+//

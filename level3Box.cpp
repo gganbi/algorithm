900eceea4983340e0389a2da68b299ec76a77e36
gganbi <gganbi@naver.com>
2020-03-11 10:57:10 +0900

    박스무네

---
+++ level3Box.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main() {
+
+	int temp[10][10] = { 0 };
+
+	int i, j;
+
+	for (i = 0; i <= 6; i++) {
+		temp[0][i] = 10;
+		temp[i][0] = 10;
+		temp[6][i] = 10;
+		temp[i][6] = 10;
+	}
+
+	for (i = 1; i <= 5; i++) {
+		for (j = 1; j <= 5; j++) {
+			scanf_s("%d", &temp[i][j]);
+		}
+	}
+	for (i = 1; i <= 5; i++) {
+		for (j = 1; j <= 5; j++) {
+
+			if (temp[i][j] < temp[i][j - 1] &&
+				temp[i][j] < temp[i - 1][j] &&
+				temp[i][j] < temp[i + 1][j] &&
+				temp[i][j] < temp[i][j + 1]
+				) {
+				printf("* ");
+			}
+			else {
+				printf("%d ", temp[i][j]);
+			}
+		}printf("\n");
+	}
+
+
+
+
+
+	return 0;
+}

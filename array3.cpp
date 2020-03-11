1985118d1c26ccca229e8df78aaa5bfb4407a829
gganbi <gganbi@naver.com>
2020-03-11 09:16:18 +0900

    좌우뒤집기문제

--- array3.cpp
+++ array3.cpp
@@ -1,35 +1,35 @@
-#include<stdio.h>
-
-int main() {
-
-	int aa[101][101] = {0};
-
-	int i = 0, j = 0;
-	int a = 0;
-	int count = 1;
-
-		
-	scanf_s("%d", &a);
-	
-
-	for (i = 1; i <= a; i++) {
-	
-		for (j = 1; j <= i; j++) {
-			aa[j][i-j+1] = count++;
-		}	
-	}
-
-	for (i = 1; i <= a; i++) {
-		for (j = 1; j <= a-i+1; j++) {
-			printf("%d ", aa[i][j]);
-		}printf("\n");
-	}
-
-	
-
-
-
-
-
-	return 0;
-}
+//#include<stdio.h>
+//
+//int main() {
+//
+//	int aa[101][101] = {0};
+//
+//	int i = 0, j = 0;
+//	int a = 0;
+//	int count = 1;
+//
+//		
+//	scanf_s("%d", &a);
+//	
+//
+//	for (i = 1; i <= a; i++) {
+//	
+//		for (j = 1; j <= i; j++) {
+//			aa[j][i-j+1] = count++;
+//		}	
+//	}
+//
+//	for (i = 1; i <= a; i++) {
+//		for (j = 1; j <= a-i+1; j++) {
+//			printf("%d ", aa[i][j]);
+//		}printf("\n");
+//	}
+//
+//	
+//
+//
+//
+//
+//
+//	return 0;
+//}
---
+++ lever3car.cpp
@@ -0,0 +1,27 @@
+//#include<stdio.h>
+//
+//int main() {
+//
+//	int aa[20] = {0};
+//
+//	int i = 0, j = 0;
+//	int a = 0;
+//	int sum = 0;
+//
+//		
+//	for (i = 1; i <=5; i++) {
+//		scanf_s("%d", &aa[i]);
+//		sum += (aa[i] * aa[i]);
+//	}
+//
+//	
+//
+//	printf("%d", sum%10);
+//	
+//
+//
+//
+//
+//
+//	return 0;
+//}

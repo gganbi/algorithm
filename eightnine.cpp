bfc038378ee1ceae9949b0c9188a7b85e44ce2d9
gganbi <gganbi@naver.com>
2020-03-11 09:36:06 +0900

    course3Offset

--- eightnine.cpp
+++ eightnine.cpp
@@ -1,32 +1,32 @@
-#include<stdio.h>
-
-int main() {
-
-	int aa[110][110] = {0};
-
-	int i = 0, j = 0;
-	int a = 0;
-	int b = 0;
-	int sum = 0;
-	scanf_s("%d", &a);
-	scanf_s("%d", &b);
-
-		
-	for (i = 1; i <=a; i++) {
-		for (j = 1; j <= b; j++) {
-			scanf_s("%d",&aa[i][j]);
-		}	
-	}
-
-	for (i = 1; i <= a; i++) {
-		for (j = b; j >= 1; j--) {
-			printf("%d ",aa[i][j]);
-		}printf("\n");
-	}
-
-
-
-
-
-	return 0;
-}
+//#include<stdio.h>
+//
+//int main() {
+//
+//	int aa[110][110] = {0};
+//
+//	int i = 0, j = 0;
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	scanf_s("%d", &a);
+//	scanf_s("%d", &b);
+//
+//		
+//	for (i = 1; i <=a; i++) {
+//		for (j = 1; j <= b; j++) {
+//			scanf_s("%d",&aa[i][j]);
+//		}	
+//	}
+//
+//	for (i = 1; i <= a; i++) {
+//		for (j = b; j >= 1; j--) {
+//			printf("%d ",aa[i][j]);
+//		}printf("\n");
+//	}
+//
+//
+//
+//
+//
+//	return 0;
+//}

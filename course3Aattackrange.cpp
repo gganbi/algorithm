4606aac1774accba8af26ac1f5a51cbd9941cc95
gganbi <gganbi@naver.com>
2020-03-13 22:00:09 +0900

    큰자릿수 더하기

--- course3Aattackrange.cpp
+++ course3Aattackrange.cpp
@@ -1,128 +1,128 @@
-#include <stdio.h>
-
-int main() {
-
-	int n;
-	int x, y, index;
-
-	int data[500][500] = { 0, };
-	scanf_s("%d", &n);
-	scanf_s("%d", &x);
-	scanf_s("%d", &y);
-	scanf_s("%d", &index);
-
-	for (int i = 0; i <= n + 1; i++) {
-		data[i][0] = 100;
-		data[0][i] = 100;
-		data[n + 1][i] = 100;
-		data[i][n + 1] = 100;
-	}
-
-
-	int sang = x-1;
-	int ha = n - x;
-	int ja = y - 1;
-	int wo = n - y;
-
-	if (x - 1 >= index) {
-		sang = index;
-	}
-
-	if (n-x>= index) {
-		ha = index;
-	}
-
-	if (y - 1 >= index) {
-		ja = index;
-	}
-
-	if (n - y >= index) {
-		wo = index;
-	}
-	
-
-
-	// 좌로직
-	for (int i = 0; i <= ja; i++) {
-
-		int temp1 = i;
-		for (int j = 0; j <= sang; j++) {
-
-			if (data[x - j][y - i] == 100) {
-				break;
-			}
-
-			data[x - j][y - i] = temp1++;
-			if (data[x - j - 1][y - i] == 100 ||
-				data[x - j][y - i] == index) {
-				break;
-			}
-		}
-
-	//	하
-		int temp2 = i;
-		for (int j = 0; j <= ha; j++) {
-
-			if (data[x + j][y - i] == 100) {
-				break;
-			}
-			data[x + j][y - i] = temp2++;
-			if (data[x + j + 1][y - i] == 100 ||
-				data[x + j][y - i] == index) {
-				break;
-			}
-		}
-
-	}
-
-	// 우로직
-	for (int i = 0; i <= wo; i++) {
-
-		int temp1 = i;
-		for (int j = 0; j <= sang; j++) {
-
-			if (data[x - j][y + i] == 100) {
-				break;
-			}
-			data[x - j][y + i] = temp1++;
-			if (data[x - j - 1][y + i] == 100 ||
-				data[x - j][y + i] == index) {
-				break;
-			}
-		}
-
-		//하
-		int temp2 = i;
-		for (int j = 0; j <= ha; j++) {
-
-			if (data[x + j][y + i] == 100) {
-				break;
-			}
-			data[x + j][y + i] = temp2++;
-			if (data[x + j + 1][y + i] == 100 ||
-				data[x + j][y + i] == index) {
-				break;
-			}
-		}
-
-	}
-
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-
-			if (i == x && j == y) {
-				printf("x ");
-			}
-			else {
-				printf("%d ", data[i][j]);
-			}
-		}
-
-		printf("\n");
-	}
-
-	return 0;
-
-}
-
+//#include <stdio.h>
+//
+//int main() {
+//
+//	int n;
+//	int x, y, index;
+//
+//	int data[500][500] = { 0, };
+//	scanf_s("%d", &n);
+//	scanf_s("%d", &x);
+//	scanf_s("%d", &y);
+//	scanf_s("%d", &index);
+//
+//	for (int i = 0; i <= n + 1; i++) {
+//		data[i][0] = 100;
+//		data[0][i] = 100;
+//		data[n + 1][i] = 100;
+//		data[i][n + 1] = 100;
+//	}
+//
+//
+//	int sang = x-1;
+//	int ha = n - x;
+//	int ja = y - 1;
+//	int wo = n - y;
+//
+//	if (x - 1 >= index) {
+//		sang = index;
+//	}
+//
+//	if (n-x>= index) {
+//		ha = index;
+//	}
+//
+//	if (y - 1 >= index) {
+//		ja = index;
+//	}
+//
+//	if (n - y >= index) {
+//		wo = index;
+//	}
+//	
+//
+//
+//	// 좌로직
+//	for (int i = 0; i <= ja; i++) {
+//
+//		int temp1 = i;
+//		for (int j = 0; j <= sang; j++) {
+//
+//			if (data[x - j][y - i] == 100) {
+//				break;
+//			}
+//
+//			data[x - j][y - i] = temp1++;
+//			if (data[x - j - 1][y - i] == 100 ||
+//				data[x - j][y - i] == index) {
+//				break;
+//			}
+//		}
+//
+//	//	하
+//		int temp2 = i;
+//		for (int j = 0; j <= ha; j++) {
+//
+//			if (data[x + j][y - i] == 100) {
+//				break;
+//			}
+//			data[x + j][y - i] = temp2++;
+//			if (data[x + j + 1][y - i] == 100 ||
+//				data[x + j][y - i] == index) {
+//				break;
+//			}
+//		}
+//
+//	}
+//
+//	// 우로직
+//	for (int i = 0; i <= wo; i++) {
+//
+//		int temp1 = i;
+//		for (int j = 0; j <= sang; j++) {
+//
+//			if (data[x - j][y + i] == 100) {
+//				break;
+//			}
+//			data[x - j][y + i] = temp1++;
+//			if (data[x - j - 1][y + i] == 100 ||
+//				data[x - j][y + i] == index) {
+//				break;
+//			}
+//		}
+//
+//		//하
+//		int temp2 = i;
+//		for (int j = 0; j <= ha; j++) {
+//
+//			if (data[x + j][y + i] == 100) {
+//				break;
+//			}
+//			data[x + j][y + i] = temp2++;
+//			if (data[x + j + 1][y + i] == 100 ||
+//				data[x + j][y + i] == index) {
+//				break;
+//			}
+//		}
+//
+//	}
+//
+//
+//	for (int i = 1; i <= n; i++) {
+//		for (int j = 1; j <= n; j++) {
+//
+//			if (i == x && j == y) {
+//				printf("x ");
+//			}
+//			else {
+//				printf("%d ", data[i][j]);
+//			}
+//		}
+//
+//		printf("\n");
+//	}
+//
+//	return 0;
+//
+//}
+//
--- stringadd.cpp
+++ stringadd.cpp
@@ -1,42 +1,95 @@
-//#include<stdio.h>
-//#include<string.h>
-//int main() {
-//
-//	char aa[1000];
-//	char bb[1000];
-//
-//	fgets(aa, 1000, stdin);
-//	fflush(stdin);
-//	fgets(bb, 1000, stdin);
-//
-//	int diff = strlen(aa) - strlen(bb);
-//
-//	if (diff>0) {
-//
-//		bb[strlen(bb) - 1 + diff] = '\0';
-//		for (int i = strlen(bb) - 2; i >= 0; i--) {
-//			bb[i + diff] = bb[i];
-//		}
-//		for (int i = 0; i < diff; i++) {
-//			bb[i] = '0' ;
-//		}
-//
-//	}
-//	else if (diff<0) {
-//
-//		aa[strlen(aa) - 1 + diff*-1] = '\0';
-//		for (int i = strlen(aa) - 2; i >= 0; i--) {
-//			aa[i + diff*-1] = aa[i];
-//		}
-//		
-//		for (int i = 0; i < diff*-1; i++) {
-//			aa[i] = '0' ;
-//		}
-//	}
-//
-//     printf("%s\n", aa);
-//	 printf("%s", bb);
-//	 
-//
-//	return 0;
-//}
+#include<stdio.h>
+#include<string.h>
+int main() {
+
+	char aa[1000];
+	char bb[1000];
+	int cnt[1000] = { 0, };
+
+	fgets(aa, 1000, stdin);
+
+	fgets(bb, 1000, stdin);
+
+	int diff = strlen(aa) - strlen(bb);
+
+
+	if (strlen(aa) == 2 && strlen(bb) == 2
+		&& aa[0]-'0' + bb[0]-'0' < 10) {
+		printf("%d", aa[0]-'0' + bb[0]-'0');
+	}
+	else {
+		if (diff > 0) {
+
+			bb[strlen(bb) - 1 + diff] = '\0';
+			for (int i = strlen(bb) - 2; i >= 0; i--) {
+				bb[i + diff] = bb[i];
+			}
+			for (int i = 0; i < diff; i++) {
+				bb[i] = '0';
+			}
+
+		}
+		else if (diff < 0) {
+
+			aa[strlen(aa) - 1 + diff * -1] = '\0';
+			for (int i = strlen(aa) - 2; i >= 0; i--) {
+				aa[i + diff * -1] = aa[i];
+			}
+
+			for (int i = 0; i < diff * -1; i++) {
+				aa[i] = '0';
+			}
+		}
+
+		int count = 1;
+
+		if (diff >= 0) {
+			for (int i = strlen(aa) - 2; i >= 0; i--) {
+				if ((aa[i] - '0' + bb[i] - '0') >= 10) {
+					cnt[count++] += ((aa[i] - '0' + bb[i] - '0') % 10);
+					cnt[count] += ((aa[i] - '0' + bb[i] - '0') / 10);
+				}
+				else {
+					cnt[count++] += (aa[i] - '0' + bb[i] - '0');
+				}
+			}
+		}
+		else {
+			for (int i = strlen(bb) - 2; i >= 0; i--) {
+				if ((aa[i] - '0' + bb[i] - '0') >= 10) {
+					cnt[count++] += ((aa[i] - '0' + bb[i] - '0') % 10);
+					cnt[count] += ((aa[i] - '0' + bb[i] - '0') / 10);
+				}
+				else {
+					cnt[count++] += (aa[i] - '0' + bb[i] - '0');
+				}
+			}
+
+
+		}
+
+
+		if (diff > 0) {
+			for (int i = strlen(aa) - 1; i >= 1; i--) {
+				printf("%d", cnt[i]);
+			}
+		}
+		else if (diff < 0) {
+			for (int i = strlen(bb) - 1; i >= 1; i--) {
+				printf("%d", cnt[i]);
+			}
+		}
+		else {
+			for (int i = strlen(aa); i >= 1; i--) {
+				printf("%d", cnt[i]);
+			}
+		}
+	
+	}
+
+	
+
+
+
+	return 0;
+}

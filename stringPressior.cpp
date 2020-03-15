c2ae259d4358ad85c3fb8e0c776e2314c038851a
gganbi <gganbi@naver.com>
2020-03-15 10:05:22 +0900

    역방향

--- stringPressior.cpp
+++ stringPressior.cpp
@@ -1,19 +1,17 @@
 //#include<stdio.h>
 //#include<string.h>
-//#include<stdlib.h>
 //
 //int main() {
 //
 //	char data[1010];
-//	char  tmp[1010] = {0,};
-//	char tmpCnt = 0;
-//	char indexCnt = 1;
-//
+//	char  tmp[1010] = { 0, };
+//	int tmpCnt = 0;
+//	int indexCnt = 1;
+//	char ddd = 3;
 //
 //	fgets(data, 1010, stdin);
 //
-//
-//	for (int i = 0; i < strlen(data) - 2; i++) {
+//	for (int i = 0; i < strlen(data) - 1; i++) {
 //		if (data[i + 1] == data[i]) {
 //			indexCnt++;
 //		}
@@ -23,29 +21,55 @@
 //				tmp[tmpCnt++] = data[i];
 //			}
 //			else {
-//				if (indexCnt / 10 ==0) {
-//					tmp[tmpCnt++] = indexCnt + '0';
+//				if (indexCnt / 10 == 0) {
+//					tmp[tmpCnt++] = indexCnt ;
 //				}
-//				else if(indexCnt / 10 < 10) {
-//					tmp[tmpCnt++] = indexCnt / 10 + '0';
-//					tmp[tmpCnt++] = indexCnt%10 + '0';
+//				else if (indexCnt / 10 < 10) {
+//					tmp[tmpCnt++] = indexCnt / 10;
+//					tmp[tmpCnt++] = indexCnt % 10;
 //				}
 //				else {
-//					tmp[tmpCnt++] = indexCnt / 100 + '0';
-//					tmp[tmpCnt++] = indexCnt % 100/10 + '0';
-//					tmp[tmpCnt++] = indexCnt % 100 % 10 + '0';
+//					tmp[tmpCnt++] = indexCnt / 100 ;
+//					tmp[tmpCnt++] = indexCnt % 100 / 10 ;
+//					tmp[tmpCnt++] = indexCnt % 100 % 10 ;
 //				}
 //
-//				
+//
 //				tmp[tmpCnt++] = data[i];
 //			}
 //			indexCnt = 1;
 //		}
 //	}
 //
-//
 //	for (int i = 0; i < tmpCnt; i++) {
 //		printf("%c", tmp[i]);
+//	}
+//
+//	char temp = data[strlen(data) - 1];
+//
+//	int cnt = 1;
+//
+//	if (strlen(data) != 1) {
+//		for (int i = strlen(data) - 1; i >= 0; i--) {
+//
+//			if (temp == data[i - 1]) {
+//				cnt++;
+//			}
+//			else {
+//				break;
+//			}
+//		}
+//
+//		if (cnt == 1) {
+//			printf("%c", temp);
+//		}
+//		else {
+//			printf("%d%c", cnt, temp);
+//		}
+//
+//	}
+//	else {
+//		printf("%c", data[0]);
 //	}
 //
 //
--- test.cpp
+++ test.cpp
@@ -1,45 +1,38 @@
-//#include<stdio.h>
-//
-//int main() {
-//
-//	int n;
-//	scanf_s("%d", &n);
-//
-//	int data[200];
-//	for (int i = 0; i < n; i++) {
-//		scanf_s("%d", &data[i]);
-//	}
-//
-//	int x, y;
-//
-//	scanf_s("%d %d", &x, &y);
-//
-//
-//	int tmp=x;
-//	if (y >= n) {
-//		y = y % n;
-//	}
-//	int tmp2=0;
-//
-//	for (int i = 1; i <= y; i++) {
-//		if (tmp + i > n - 1) {
-//			tmp = 0;
-//			tmp2 = 0;
-//		}
-//		else {
-//			tmp2++;
-//		}
-//	}
-//	tmp += tmp2;
-//
-//	printf("%d", data[tmp]);
-//			
-//	
-//	
-//	
-//
-//
-//	return 0;
-//}
-//
-//
+#include<stdio.h>
+
+int main() {
+
+	int n;
+	scanf_s("%d", &n);
+
+	int data[101];
+	for (int i = 0; i < n; i++) {
+		scanf_s("%d", &data[i]);
+	}
+
+	int a, b;
+	scanf_s("%d %d", &a, &b);
+
+	if (b >= n); {
+		b %= n;
+	}
+
+	int index = a;
+	int temp = a;;
+	//for (int i = 1; i <= b; i++) {
+		if (a - b < 0) {
+			index = n+(a - b);
+		}
+		else {
+			index -= b;
+		}
+		
+		printf("%d", data[index]);
+	//}
+	
+
+
+	return 0;
+}
+
+

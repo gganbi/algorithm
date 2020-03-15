bd8349bdfcba662f9078b25955951d0e848e24f2
gganbi <gganbi@naver.com>
2020-03-15 11:15:43 +0900

    상하좌우

--- test.cpp
+++ test.cpp
@@ -1,54 +1,35 @@
 #include <stdio.h>
 
 int main() {
-	int n, q;
-	int data[101] = {0,};
+	int n, m;
+	int data[101][101];
 
-	int aa[101] = { 0, };
-	int bb[101] = { 0, };
+	int dx[4] = { 0,0,1,-1};
+	int dy[4] = { 1,-1,0,0 };
 
-	scanf_s("%d", &n);
-	scanf_s("%d", &q);
-
-	for (int i = 0; i <n; i++) {
-		scanf_s("%d", &data[i]);
-	}
-	for (int i = 0; i < q; i++) {
-		scanf_s("%d %d", &aa[i], &bb[i]);
-	}
-	int index = 0;
-	for (int i = 0; i < q; i++) {
-		
-		if (bb[i] / n >= 1) {
-			bb[i] %= n;
+	scanf_s("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf_s("%d", &data[i][j]);
 		}
-		if (aa[i] == 1) {
-			if (index + bb[i] > n - 1) {
-				index = bb[i]-(n-1-index)-1;
-			}
-			else {
-				index += bb[i];
-			}
-
-			printf("%d\n", data[index]);
-		}
-		else {
-			if (index - bb[i] < 0) {
-				index = n +(index - bb[i]);
-			}
-			else {
-				index -= bb[i];
-			}
-			printf("%d\n", data[index]);
-		
-		}
-		
 	}
+	int y, x, d, r;
 
+	scanf_s("%d %d %d %d", &y, &x, &d, &r);
 
-
+	if (d == 0) {
+		printf("%d",data[y][x+r] );
+	
+	}
+	else if (d == 1) {
+		printf("%d", data[y][x - r]);
+	}
+	else if (d == 2) {
+		printf("%d", data[y+r][x]);
+	}
+	else {
+		printf("%d", data[y-r][x + r]);
+	}
 
 	return 0;
-
-
 }

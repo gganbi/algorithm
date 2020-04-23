cdbdf76e6c280355c5e5031624bbee330c26817a
gganbi <gganbi@naver.com>
2020-04-24 08:43:19 +0900

    20200424

--- factorialBinary.cpp
+++ factorialBinary.cpp
@@ -1,24 +1,59 @@
-//#include <stdio.h>
+////#include <stdio.h>
+////
+////
+////void factorial(int a) {
+////
+////	if (a / 2 == 0) {
+////		printf("1");
+////	}
+////	else {
+////		factorial(a / 2);
+////		printf("%d", a % 2);
+////	}
+////}
+////
+////int main() {
+////
+////	int a;
+////
+////	scanf_s("%d", &a);
+////	factorial(a);
+////
+////
+////	return 0;
+////}
 //
 //
-//void factorial(int a) {
+//#include<stdio.h>
 //
-//	if (a / 2 == 0) {
+//int n;
+//
+//int data;
+//
+//int result[100];
+//int count = 0;
+//
+//void getResult(int n) {
+//
+//	if (n == 1) {
 //		printf("1");
+//		return;
 //	}
-//	else {
-//		factorial(a / 2);
-//		printf("%d", a % 2);
-//	}
-//}
+//	getResult(n - 1);
 //
+//	printf("%d", n);
+//	getResult(n - 1);
+//
+//
+//	
+//}
 //int main() {
 //
-//	int a;
+//	scanf_s("%d", &n);
+//	getResult(n);
+//	/*for (int i = count; i >= 0; i--) {
+//		printf("%d ", result[i]);
+//	}*/
+////	printf("%d", getResult(0));
 //
-//	scanf_s("%d", &a);
-//	factorial(a);
-//
-//
-//	return 0;
 //}
---
+++ move미해갸스.cpp
@@ -0,0 +1,11 @@
+#include<cstdio>
+using namespace std;
+
+
+
+int n, m, k, t;
+int y, x, d;
+
+int main() {
+
+}

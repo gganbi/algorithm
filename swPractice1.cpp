f12bad340a96419a07822dcf911a2bed474c8061
gganbi <gganbi@naver.com>
2020-04-27 08:14:11 +0900

    20200427

--- swPractice1.cpp
+++ swPractice1.cpp
@@ -1,3 +1,18 @@
-#include <cstdio>
-
-using namespace std;
+//#include<cstdio>
+//#include<vector>
+//#include<queue>
+//#include<string.h>
+//#include<stdlib.h>
+//#include<algorithm>
+//using namespace std;
+//using namespace std;
+//
+//int main() {
+//
+//	//int a = 3;
+//	abs(a);
+//
+//
+//
+//
+//}
--- swPractice4SLIKARcpp.cpp
+++ swPractice4SLIKARcpp.cpp
@@ -1,207 +1,50 @@
 //#include<cstdio>
 //#include<queue>
+//#include<algorithm>
+//#include<string.h>
+//#include<stdlib.h>
+//
 //
 //using namespace std;
 //
-//
-//int r, c;
-//char dataa[101][101];
 //bool waterCheck[101][101] = { false, };
 //bool gosmCheck[101][101] = { false, };
 //
 //
-//int cnt = 0;
+//
+//struct dataSet {
+//	int x;
+//	int y;
+//	int cnt;
+//	char value;
+//};
 //
 //int dx[4] = { -1,1,0,0 };
 //int dy[4] = { 0,0,-1,1 };
 //
-//bool vari = false;
+//int lastX, lastY;
 //
-//int cntt = 0;
+//int r, c;
 //
+//char dataa[200][200];
 //
-//int bibuX, bibuY;//고슴도치
+//void bfs() {
 //
-//int lastX, lastY; // 비버위치
-//
-//struct gosm {
-//	int x;
-//	int y;
-//	int cnt;
-//};
-//
-//void bfs(int x,int y) {
-//	gosm Gosm;
-// 
-//	Gosm.x = x;
-//	Gosm.y = y;
-//	Gosm.cnt = 0;
-//
-//	queue<gosm> test;
-//	test.push(Gosm);
-//
-//	int cntt = 0;
-//
-//	while (!test.empty()) {
-//
-//		for (int i = 1; i <= r; i++) {  //물차오르는것
-//			for (int j = 1; j <= c; j++) {
-//				if (dataa[i][j] == '*') {
-//					if (waterCheck[i][j] == false) {
-//						for (int m = 0; m < 4; m++) {
-//							int x = i + dx[m];
-//							int y = j + dy[m];
-//							if (dataa[x][y] == '.' || dataa[x][y] == 'S') {
-//								dataa[x][y] = '*';
-//								waterCheck[x][y] = true;
-//							}
-//						}
-//					}
-//				}
-//			}
-//		}
-//
-//		for (int i = 1; i <= r; i++) {
-//			for (int j = 1; j <= c; j++) {
-//				waterCheck[i][j] = false;
-//			}
-//		}
-//
-//
-//		for (int i = 1; i <= r; i++) {  //물차오르는것
-//			for (int j = 1; j <= c; j++) {
-//				if (dataa[i][j] == 'S') {
-//					if (gosmCheck[i][j] == false) {
-//						for (int m = 0; m < 4; m++) {
-//							int x = i + dx[m];
-//							int y = j + dy[m];
-//							if (dataa[x][y] == '.') {
-//								dataa[x][y] = 'S';
-//								gosmCheck[x][y] = true;
-//							}
-//						}
-//					}
-//				}
-//			}
-//		}
-//
-//		for (int i = 1; i <= r; i++) {
-//			for (int j = 1; j <= c; j++) {
-//				gosmCheck[i][j] = false;
-//			}
-//		}
-//
-//		/*printf("\n");
-//		for (int i = 1; i <= r; i++) {
-//			for (int j = 1; j <= c; j++) {
-//				printf("%c ", dataa[i][j]);
-//			}printf("\n");
-//		}*/
-//	
-//		/*gosm cur = test.front();
-//		test.pop();
-//
-//
-//		cntt++;
-//		gosm input;
-//		int tmpCnt = 0;
-//		for (int i = 0; i < 4; i++) {
-//			int xx = cur.x + dx[i];
-//			int yy = cur.y + dy[i];
-//
-//			if (dataa[xx][yy] == 'D') {
-//				printf("%d\n", cur.cnt+1);
-//				vari = true;
-//				return;
-//			}
-//
-//			
-//			input.x = xx;
-//			input.y = yy;
-//			input.cnt = cur.cnt +1;
-//
-//			if (dataa[xx][yy] == '.') {
-//				test.push(input);
-//				dataa[xx][yy] = 'S';
-//			}
-//			else {
-//				tmpCnt++;
-//			}
-//		}*/
-//
-//
-//	
-//		printf("\n");
-//		for (int i = 1; i <= r; i++) {
-//			for (int j = 1; j <= c; j++) {
-//				printf("%c ", dataa[i][j]);
-//			}printf("\n");
-//		}
-//		printf("\nfffff %d %d %d\n", input.x, input.y, input.cnt);
-//		
-//		
-//		/*for (int i = 1; i <= r; i++) {
-//			for (int j = 1; j <= c; j++) {
-//				if (dataa[i][j] == 'S') {
-//
-//					for (int iii = 0; iii < 4; iii++) {
-//						int x = i + dx[iii];
-//						int y = j + dy[iii];
-//
-//						if (dataa[x][y] == 'D') {
-//							printf("%d", input.cnt + 1);
-//							vari = true;
-//							return;
-//						}
-//					}
-//
-//				}
-//			}
-//		}*/
-//
-//
-//		int cntj = 0;
-//
-//		for (int i = 0; i < 4; i++) {
-//		
-//			int x = lastX + dx[i];
-//			int y = lastY + dy[i];
-//
-//
-//			if (dataa[x][y] == '*' || dataa[x][y] == '\0') {
-//				cntj++;
-//			}
-//		}
-//
-//		printf("%d %c \n",cntj,dataa[0][1]);
-//
-//		if (cntj == 4) {
-//			
-//			printf("KAKTUS");
-//				return;
-//		
-//		}
-//
-//	
-//	}
-//
-//}
-//
-//
-//int cnttt = 0;
-//int main() {
-//
-//
-//	scanf("%d %d", &r, &c);
+//	queue<dataSet> myQue;
 //
 //
 //	for (int i = 1; i <= r; i++) {
 //		for (int j = 1; j <= c; j++) {
-//			scanf(" %c", &dataa[i][j]);
-//			if (dataa[i][j] == 'S') {
-//				bibuX = i;
-//				bibuY = j;
+//			if (dataa[i][j] == '*' || dataa[i][j] == 'S') {
+//				dataSet tt;
+//				tt.x = i;
+//				tt.y = j;
+//				tt.cnt = 0;
+//				tt.value = dataa[i][j];
+//				myQue.push(tt);
+//
 //			}
+//			
 //			if (dataa[i][j] == 'D') {
 //				lastX = i;
 //				lastY = j;
@@ -209,129 +52,161 @@
 //		}
 //	}
 //
+//	while (!myQue.empty()) {
+//	
+//		dataSet cur = myQue.front();
+//		myQue.pop();
+//		printf("\n cur :  %d %d %c \n", cur.x, cur.y,dataa[cur.x][cur.y]);
 //
-//	bfs(bibuX, bibuY);
-//	while (true) {
-//		cnttt++;
+//		if (dataa[cur.x][cur.y] == 'S') {	
 //
-//
-//		int cntj3 = 0;
-//
-//		for (int i = 0; i < 4; i++) {
-//
-//			int x = lastX + dx[i];
-//			int y = lastY + dy[i];
+//		//	printf("\n cur :  %d %d\n", cur.x, cur.y);
 //
 //
-//			if (dataa[x][y] == '*' || dataa[x][y] == '\0' || dataa[x][y] == 'X') {
-//				cntj3++;
+//			for (int ii = 0; ii < 4; ii++) {
+//			
+//			/*	if (cur.x == 3 && cur.y == 1) {
+//					printf("\nggd\n");
+//
+//					for (int i = 1; i <= r; i++) {
+//						for (int j = 1; j <= c; j++) {
+//							printf("%c ", dataa[i][j]);
+//						}
+//						printf("\n");
+//					}
+//				}*/
+//				int xx = cur.x + dx[ii];
+//				int yy = cur.y + dy[ii];
+//
+//				if (dataa[xx][yy] == '.') {
+//					printf("sss\n");
+//					dataSet tmp;
+//					tmp.x = xx;
+//					tmp.y = yy;
+//					tmp.cnt = cur.cnt + 1;
+//				//	tmp.value = 'S';
+//					dataa[xx][yy] = 'S';
+//					myQue.push(tmp);  //한번더 체킹
+//
+//					printf("\ss\n");
+//
+//					for (int i = 1; i <= r; i++) {
+//						for (int j = 1; j <= c; j++) {
+//							printf("%c ", dataa[i][j]);
+//						}
+//						printf("\n");
+//					}
+//
+//					//printf("\n tmp :  %d %d\n", tmp.x, tmp.y);
+//
+//					for (int iii = 0; iii < 4; iii++) {
+//
+//						int xxx = tmp.x + dx[iii];
+//						int yyy = tmp.y + dy[iii];
+//						if (dataa[xxx][yyy] == 'D') {
+//							printf("AAAAAAAA%d", tmp.cnt + 1);
+//							return;
+//						}
+//					}
+//
+//
+//
+//				}
+//				else if (dataa[xx][yy] == 'D') {		
+//					printf("AAAAAAAA%d", cur.cnt + 1);
+//					return;
+//				}
 //			}
 //		}
+//		else if (dataa[cur.x][cur.y] == '*') {
+//			for (int ii = 0; ii < 4; ii++) {
 //
+//				int xx = cur.x + dx[ii];
+//				int yy = cur.y + dy[ii];
 //
-//		if (cntj3 == 4) {
+//				if (dataa[xx][yy] == '.' || dataa[xx][yy] == 'S') {
 //
-//			printf("KAKTUS");
-//			break;
+//					printf("ddd\n");
+//					dataSet tmp;
+//					tmp.x = xx;
+//					tmp.y = yy;
+//					tmp.cnt = cur.cnt + 1;
+//				//	tmp.value = '*';
 //
-//		}
+//					dataa[xx][yy] = '*';
+//					myQue.push(tmp);
+//					printf("\dd\n");
 //
-//
-//		for (int i = 1; i <= r; i++) {  //물차오르는것
-//			for (int j = 1; j <= c; j++) {
-//				if (dataa[i][j] == '*') {
-//					if (waterCheck[i][j] == false) {
-//						for (int m = 0; m < 4; m++) {
-//							int x = i + dx[m];
-//							int y = j + dy[m];
-//							if (dataa[x][y] == '.' || dataa[x][y] == 'S') {
-//								dataa[x][y] = '*';
-//								waterCheck[x][y] = true;
-//							}
+//					for (int i = 1; i <= r; i++) {
+//						for (int j = 1; j <= c; j++) {
+//							printf("%c ", dataa[i][j]);
 //						}
+//						printf("\n");
 //					}
 //				}
 //			}
 //		}
-//
-//		for (int i = 1; i <= r; i++) {
-//			for (int j = 1; j <= c; j++) {
-//				waterCheck[i][j] = false;
-//			}
-//		}
-//		int cntj = 0;
-//
-//		for (int i = 0; i < 4; i++) {
-//
-//			int x = lastX + dx[i];
-//			int y = lastY + dy[i];
-//
-//
-//			if (dataa[x][y] == '*' || dataa[x][y] == '\0' || dataa[x][y] == 'X') {
-//				cntj++;
-//			}
-//		}
-//
-//
-//		if (cntj == 4) {
-//
-//			printf("KAKTUS");
-//			break;
+//		else if (dataa[cur.x][cur.y] == 'D') {
+//			printf("%d", cur.cnt + 1);
+//			return;
 //
 //		}
-//
-//
-//		for (int i = 1; i <= r; i++) {  //고슴도치
-//			for (int j = 1; j <= c; j++) {
-//				if (dataa[i][j] == 'S') {
-//					if (gosmCheck[i][j] == false) {
-//						for (int m = 0; m < 4; m++) {
-//							int x = i + dx[m];
-//							int y = j + dy[m];
-//							if (dataa[x][y] == '.') {
-//								dataa[x][y] = 'S';
-//								gosmCheck[x][y] = true;
-//							}
-//						}
-//					}
-//				}
-//			}
-//		}
-//
-//		for (int i = 1; i <= r; i++) {
-//			for (int j = 1; j <= c; j++) {
-//				gosmCheck[i][j] = false;
-//			}
-//		}
-//
-//
-//		bool cntjj = false;
-//		for (int i = 0; i < 4; i++) {
-//
-//			int x = lastX + dx[i];
-//			int y = lastY + dy[i];
-//
-//			if (dataa[x][y] == 'S') {
-//				cntjj = true;
-//				break;
-//			}
-//		}
-//
-//		if (cntjj == true) {
-//
-//			if (cnttt == 1) {
-//				printf("%d", cnttt);
-//			}
-//			else {
-//
-//				printf("%d", cnttt + 1);
-//			}
-//			break;
-//
-//		}
-//
 //	}
 //
+//	int lastCnt = 0;
+//	//Q 다돌고 걔위치확인
+//	for (int ii = 0; ii < 4; ii++) {
+//
+//
+//		
+//		int xx = lastX +dx[ii];
+//		int yy = lastY + dy[ii];
+//		
+//
+//		if (dataa[xx][yy] == '*' || dataa[xx][yy] == '\0' || dataa[xx][yy] == 'X') {
+//			lastCnt++;
+//		}
+//	}
+//
+//	if (lastCnt == 4) {
+//		printf("KAKTUS");
+//		printf("\n %d %d \n", lastX, lastY);
+//		return;
+//	}
+//
+//
+//
+//	
+//	
+//
+//
+//
+//}
+//
+//
+//
+//int main() {
+//
+//	scanf_s("%d %d",&r,&c);
+//
+//
+//	for (int i = 1; i <= r; i++) {
+//		for (int j = 1; j <= c; j++) {
+//			scanf_s(" %c",&dataa[i][j]);
+//		}
+//	}
+//
+//
+//	/*
+//
+//	for (int i = 1; i <= r; i++) {
+//		for (int j = 1; j <= c; j++) {
+//			printf("%c ",dataa[i][j]);
+//		}
+//		printf("\n");
+//	}*/
+//
+//	bfs();
 //
 //
 //}
